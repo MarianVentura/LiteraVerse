929e5f7ae4a829f7468902b2d660fdfafe7a95a4
MarianVentura <marianv1119@gmail.com>
2025-11-18 17:45:47 -0400

    Se ha creado el módulo `ApiModule` para la inyección de dependencias de red.

---
+++ app/src/main/java/edu/ucne/literaverse/di/ApiModule.kt
@@ -0,0 +1,48 @@
+package edu.ucne.literaverse.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.literaverse.data.remote.LiteraVerseApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://franrianela-api-g8ddh7bmfegddcdh.centralus-01.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLiteraVerseApi(moshi: Moshi, okHttpClient: OkHttpClient): LiteraVerseApi =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+            .create(LiteraVerseApi::class.java)
+}
