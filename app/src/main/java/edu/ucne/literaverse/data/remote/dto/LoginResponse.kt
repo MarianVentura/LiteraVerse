931308de07f60edd6aaf990b3f226ccb846717b7
MarianVentura <marianv1119@gmail.com>
2025-11-27 20:47:39 -0400

    Se ha modificado el DTO `LoginResponse`.
    Se eliminó el campo `password` y se añadieron en su lugar `token` y `loginDate` para manejar la respuesta de la API tras un inicio de sesión exitoso.

--- app/src/main/java/edu/ucne/literaverse/data/remote/dto/LoginResponse.kt
+++ app/src/main/java/edu/ucne/literaverse/data/remote/dto/LoginResponse.kt
@@ -3,5 +3,6 @@ package edu.ucne.literaverse.data.remote.dto
 data class LoginResponse(
     val usuarioId: Int,
     val userName: String,
-    val password: String
+    val token: String,
+    val loginDate: String
 )
