47d5f9607b928584b2d2813040855c6f3888f266
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-01 18:12:00 -0400

    Refactor♻️: Agregando ruta para LibraryScreen en MainNavigation

--- app/src/main/java/edu/ucne/literaverse/presentation/navigation/MainNavigation.kt
+++ app/src/main/java/edu/ucne/literaverse/presentation/navigation/MainNavigation.kt
@@ -115,6 +115,7 @@ fun MainNavigation(navController: NavHostController) {
             )
         }
 
+
         composable<Screen.Library> {
             LibraryScreen(
                 onNavigateToHome = {
