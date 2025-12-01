9f05d2bd3afa0bc557dc8ca6b29c8f43d4ee6f20
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-01 17:54:33 -0400

    refactor♻: Renombrar Novel a Story en SearchRepository
    Modificar los métodos de búsqueda y observación en `SearchRepository` para utilizar el tipo `Story` en lugar de `Novel`, reflejando cambios en el dominio.

--- app/src/main/java/edu/ucne/literaverse/domain/repository/SearchRepository.kt
+++ app/src/main/java/edu/ucne/literaverse/domain/repository/SearchRepository.kt
@@ -1,13 +1,17 @@
 package edu.ucne.literaverse.domain.repository
 
+
 import edu.ucne.literaverse.data.remote.Resource
-import edu.ucne.literaverse.domain.model.Novel
 import edu.ucne.literaverse.domain.model.SearchFilters
+import edu.ucne.literaverse.domain.model.Story
 import kotlinx.coroutines.flow.Flow
 
+
 interface SearchRepository {
-    suspend fun searchNovels(filters: SearchFilters): Resource<List<Novel>>
-    suspend fun getPopularNovels(): Resource<List<Novel>>
-    suspend fun getRecentNovels(): Resource<List<Novel>>
-    fun observeSearchResults(): Flow<List<Novel>>
+    suspend fun searchStories(filters: SearchFilters): Resource<List<Story>>
+    suspend fun getPopularStories(): Resource<List<Story>>
+    suspend fun getRecentStories(): Resource<List<Story>>
+    fun observeSearchResults(): Flow<List<Story>>
 }
+
+
