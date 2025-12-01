cbbf79812acdda503f4ffe83ab3b405fcb411ed6
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-01 18:55:59 -0400

    feat✨: Agregar modelo StoryReader en el dominio

---
+++ app/src/main/java/edu/ucne/literaverse/domain/model/StoryReader.kt
@@ -0,0 +1,15 @@
+package edu.ucne.literaverse.domain.model
+
+data class StoryReader(
+    val storyId: Int,
+    val title: String,
+    val synopsis: String,
+    val coverImageUrl: String?,
+    val author: String,
+    val authorId: Int,
+    val genre: String?,
+    val tags: String?,
+    val viewCount: Int,
+    val likeCount: Int,
+    val publishedChapters: List<Chapter>
+)
