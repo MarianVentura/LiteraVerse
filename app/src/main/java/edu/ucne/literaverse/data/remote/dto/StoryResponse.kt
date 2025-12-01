99229e84f0150438560f195cca1c7d8728c3e9ed
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-01 18:10:37 -0400

    refactor♻️: Agregar campo userName a StoryResponse.kt

--- app/src/main/java/edu/ucne/literaverse/data/remote/dto/StoryResponse.kt
+++ app/src/main/java/edu/ucne/literaverse/data/remote/dto/StoryResponse.kt
@@ -1,8 +1,10 @@
 package edu.ucne.literaverse.data.remote.dto
 
+
 data class StoryResponse(
     val storyId: Int,
     val userId: Int,
+    val userName: String?,
     val title: String,
     val synopsis: String,
     val coverImageUrl: String?,
