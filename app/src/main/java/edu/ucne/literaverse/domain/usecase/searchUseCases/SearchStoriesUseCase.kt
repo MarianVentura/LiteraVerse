2b70116285a7ed3667af5f30f008d71b9beca687
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-01 18:03:04 -0400

    refactor♻️: Renombrar SearchNovelsUseCase a SearchStoriesUseCase

--- app/src/main/java/edu/ucne/literaverse/domain/usecase/searchUseCases/SearchStoriesUseCase.kt
+++ app/src/main/java/edu/ucne/literaverse/domain/usecase/searchUseCases/SearchStoriesUseCase.kt
@@ -1,15 +1,19 @@
 package edu.ucne.literaverse.domain.usecase.searchUseCases
 
+
 import edu.ucne.literaverse.data.remote.Resource
-import edu.ucne.literaverse.domain.model.Novel
 import edu.ucne.literaverse.domain.model.SearchFilters
+import edu.ucne.literaverse.domain.model.Story
 import edu.ucne.literaverse.domain.repository.SearchRepository
 import javax.inject.Inject
 
-class SearchNovelsUseCase @Inject constructor(
+
+class SearchStoriesUseCase @Inject constructor(
     private val repository: SearchRepository
 ) {
-    suspend operator fun invoke(filters: SearchFilters): Resource<List<Novel>> {
-        return repository.searchNovels(filters)
+    suspend operator fun invoke(filters: SearchFilters): Resource<List<Story>> {
+        return repository.searchStories(filters)
     }
 }
+
+
