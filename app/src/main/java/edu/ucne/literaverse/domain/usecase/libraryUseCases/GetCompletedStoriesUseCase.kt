26f2d4c765df1016567be8b0d53aff3cd84bd3db
MarianVentura <marianv1119@gmail.com>
2025-12-02 22:22:29 -0400

    feat:✨ Se ha creado el caso de uso `GetCompletedStoriesUseCase`.
    Este caso de uso se encarga de obtener la lista de historias completadas por un usuario específico, delegando la llamada al `LibraryRepository`.

---
+++ app/src/main/java/edu/ucne/literaverse/domain/usecase/libraryUseCases/GetCompletedStoriesUseCase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.literaverse.domain.usecase.libraryUseCases
+
+import edu.ucne.literaverse.domain.model.StoryWithProgress
+import edu.ucne.literaverse.domain.repository.LibraryRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCompletedStoriesUseCase @Inject constructor(
+    private val repository: LibraryRepository
+) {
+    operator fun invoke(userId: Int): Flow<List<StoryWithProgress>> {
+        return repository.getCompletedStories(userId)
+    }
+}
