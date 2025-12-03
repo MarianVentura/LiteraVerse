9ddd14d6d1bd1295ae369e3df214faf7d0cb4bf7
MarianVentura <marianv1119@gmail.com>
2025-12-02 22:20:20 -0400

    feat:✨ Se ha creado el caso de uso `AddFavoriteUseCase`.
    Este caso de uso invoca la función `addFavorite` del `LibraryRepository` para agregar una historia a los favoritos de un usuario, pasando el `userId` y el `storyId`.

---
+++ app/src/main/java/edu/ucne/literaverse/domain/usecase/libraryUseCases/AddFavoriteUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.literaverse.domain.usecase.libraryUseCases
+
+import edu.ucne.literaverse.data.remote.Resource
+import edu.ucne.literaverse.domain.repository.LibraryRepository
+import javax.inject.Inject
+
+class AddFavoriteUseCase @Inject constructor(
+    private val repository: LibraryRepository
+) {
+    suspend operator fun invoke(userId: Int, storyId: Int): Resource<Unit> {
+        return repository.addFavorite(userId, storyId)
+    }
+}
