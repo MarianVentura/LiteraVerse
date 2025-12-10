f22f6332e7e8f7280b855a796074b1399273a5b7
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-12-10 14:29:53 -0400

    refactor ♻️: Simplifica la configuración DI del módulo ChapterRepository

--- app/src/main/java/edu/ucne/literaverse/di/ChapterRepositoryModule.kt
+++ app/src/main/java/edu/ucne/literaverse/di/ChapterRepositoryModule.kt
@@ -6,14 +6,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.literaverse.data.repository.ChapterRepositoryImpl
 import edu.ucne.literaverse.domain.repository.ChapterRepository
-import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class ChapterRepositoryModule {
+interface ChapterRepositoryModule {
+
     @Binds
-    @Singleton
-    abstract fun bindChapterRepository(
-        chapterRepositoryImpl: ChapterRepositoryImpl
+    fun bindChapterRepository(
+        impl: ChapterRepositoryImpl
     ): ChapterRepository
 }
